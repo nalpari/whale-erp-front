0b79b3eb936b9f07b2ced3e4f865b373cf8a2b18
nalpari <yoo32767@gmail.com>
2026-02-04 16:10:53 +0900

    feat: 지원 메뉴 아이콘 경로를 S3 URL로 업데이트하고 각 메뉴에 맞는 아이콘을 적용했습니다.

--- src/data/SupportMenu.ts
+++ src/data/SupportMenu.ts
@@ -10,25 +10,25 @@ export const SupportMenu: SupportMenuItem[] = [
   {
     id: 1,
     name: '요금안내/변경',
-    icon: 'lnb_menu_icon00.svg',
+    icon: 'https://whale-erp-files.s3.ap-northeast-2.amazonaws.com/assets/program_icons/lnb_menu_icon01.svg',
     link: '#',
   },
   {
     id: 2,
     name: '부가서비스 신청',
-    icon: 'lnb_menu_icon00.svg',
+    icon: 'https://whale-erp-files.s3.ap-northeast-2.amazonaws.com/assets/program_icons/lnb_menu_icon02.svg',
     link: '#',
   },
   {
     id: 3,
     name: '공지사항',
-    icon: 'lnb_menu_icon00.svg',
+    icon: 'https://whale-erp-files.s3.ap-northeast-2.amazonaws.com/assets/program_icons/lnb_menu_icon03.svg',
     link: '#',
   },
   {
     id: 4,
     name: '문의하기',
-    icon: 'lnb_menu_icon00.svg',
+    icon: 'https://whale-erp-files.s3.ap-northeast-2.amazonaws.com/assets/program_icons/lnb_menu_icon04.svg',
     link: '#',
   },
 ]
