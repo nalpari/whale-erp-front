1325cef303d2e89811879ab28a61b247d1d9a476
seulda <amekimm@gmail.com>
2026-03-31 10:44:53 +0900

    style: 오탈자 수정

--- src/components/system/authority/AuthoritySearch.tsx
+++ src/components/system/authority/AuthoritySearch.tsx
@@ -90,7 +90,7 @@ export default function AuthoritySearch({
 
   return (
     <div className={`search-wrap ${searchOpen ? '' : 'act'}`}>
-      <div className="searh-result-wrap">
+      <div className="search-result-wrap">
         <div className="search-result">
           검색결과 <span>{resultCount}건</span>
         </div>
